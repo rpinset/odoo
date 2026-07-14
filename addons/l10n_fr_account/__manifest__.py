9c17c8bfe211aa733697c558c6bc22761372e021
rpinset <12782314+rpinset@users.noreply.github.com>
2026-07-14 18:02:27 +0200

    [IMP] l10n_fr_pdp,l10n_fr_account,account_edi_ubl_cii: legal mentions + SuperPDP alignment
    Emit mandatory French legal mentions (R.123-237, BR-FR-05/06/07) with BT-33
    CompanyLegalForm, structured AAB discount note, and SuperPDP-compliant exports
    (ProfileID/BT-23, CII SIREN 9 digits, BR-FR-06 deduplication, xpath install fix).

--- addons/l10n_fr_account/__manifest__.py
+++ addons/l10n_fr_account/__manifest__.py
@@ -41,6 +41,7 @@ configuration of their taxes and fiscal positions manually.
         'data/tax_report_data.xml',
         'views/report_invoice.xml',
         'views/res_partner_views.xml',
+        'views/res_company_views.xml',
         'wizard/account_fr_fec_export_wizard_view.xml',
         'security/ir.model.access.csv',
         'data/res.bank.csv',
--- addons/l10n_fr_account/models/res_company.py
+++ addons/l10n_fr_account/models/res_company.py
@@ -9,3 +9,30 @@ class ResCompany(models.Model):
 
     l10n_fr_rounding_difference_loss_account_id = fields.Many2one('account.account', check_company=True)
     l10n_fr_rounding_difference_profit_account_id = fields.Many2one('account.account', check_company=True)
+    l10n_fr_legal_form = fields.Selection(
+        selection=[
+            ('ei', "EI"),
+            ('eurl', "EURL"),
+            ('sarl', "SARL"),
+            ('sas', "SAS"),
+            ('sasu', "SASU"),
+            ('sa', "SA"),
+            ('snc', "SNC"),
+            ('sci', "SCI"),
+            ('scs', "SCS"),
+            ('sca', "SCA"),
+            ('scop', "SCOP"),
+            ('association', "Association loi 1901"),
+            ('other', "Other"),
+        ],
+        string="Legal Form (FR)",
+        help="Legal form of the company. Used to build the mandatory legal mention "
+             "on invoices (Art. R.123-237 of the Code de commerce).",
+    )
+    l10n_fr_share_capital = fields.Monetary(
+        string="Share Capital",
+        currency_field='currency_id',
+        help="Share capital of the company. Used to build the mandatory legal mention "
+             "on invoices when the legal form is a company with share capital "
+             "(Art. R.123-237 of the Code de commerce).",
+    )
